c10e5cf4ee3ed18c7d6f981c55cf36cfc74e152e
DadasoBangar <dadasobanagar0906@gmail.com>
2023-11-25 20:47:26 +0530

    demo commit for genkins

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String getMessage()
+    {
+        return "This first GENKINS application";
+    }
+
+}
