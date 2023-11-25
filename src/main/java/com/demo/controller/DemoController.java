40776ac778e86aa3ef89bde38008be1c05f67909
SwapnaliBanagar <145130857+SwapnaliBanagar@users.noreply.github.com>
2023-11-25 23:50:49 +0530

    Update DemoController.java
    removed demo from getmapping

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/demo")
+    @GetMapping("/")
     public String getMessage()
     {
         return "This first GENKINS application";
